e88b3190e923c564b9ec07e285497ab21f9bf033
JoaoVitorML-BR <jvmlira33@gmail.com>
2025-11-20 17:21:17 -0300

    chore: inital go-worker-api

---
+++ go-worker-api/cmd/worker/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/JoaoVitorML-BR/joao-vitor-desafio-gdash-2025-02/infra/server"
+)
+
+func main() {
+	server.Start()
+}
---
+++ go-worker-api/infra/server/server.go
@@ -0,0 +1,17 @@
+package server
+
+import (
+    "net/http"
+    "github.com/gin-gonic/gin"
+)
+
+func Start() {
+    r := gin.New()
+    r.Use(gin.Recovery(), gin.Logger())
+
+    r.GET("/", func(c *gin.Context) {
+        c.String(http.StatusOK, "OK")
+    })
+
+    r.Run(":8001")
+}
