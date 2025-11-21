6ef00f5bfe608a976066adfa127092342a4e553e
JoaoVitorML-BR <jvmlira33@gmail.com>
2025-11-21 20:58:23 -0300

    chore: remove arquivo legacy consumerController.go

--- go-worker-api/app/controller/consumerController.go
+++
@@ -1,7 +0,0 @@
-package controller
-
-import "github.com/JoaoVitorML-BR/joao-vitor-desafio-gdash-2025-02/app/service"
-
-func FetchOneMessage(amqpURL, queue string) (string, error) {
-	return service.GetOneMessage(amqpURL, queue)
-}
