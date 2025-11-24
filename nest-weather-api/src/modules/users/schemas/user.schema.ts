ab7940b4494f31b80bdcd8ab133069cccd80cbbb
JoaoVitorML-BR <jvmlira33@gmail.com>
2025-11-24 11:31:53 -0300

    feat(users): add User schema with role-based access control

---
+++ nest-weather-api/src/modules/users/schemas/user.schema.ts
@@ -0,0 +1,34 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document & {
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export enum UserRole {
+  ADMIN = 'admin',
+  USER = 'user',
+}
+
+@Schema({
+  timestamps: true,
+})
+export class User {
+  @Prop({ required: true, trim: true })
+  name: string;
+
+  @Prop({ required: true, unique: true, lowercase: true, trim: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ type: String, enum: UserRole, default: UserRole.USER })
+  role: UserRole;
+
+  @Prop({ default: undefined })
+  approved?: boolean;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
