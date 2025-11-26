40648842c3a5d5ac3d20a5adcfa83ac0e12b8f60
JoaoVitorML-BR <jvmlira33@gmail.com>
2025-11-26 12:16:24 -0300

    feat: add role field with validation to update user DTO

--- nest-weather-api/src/modules/users/dto/update-user.dto.ts
+++ nest-weather-api/src/modules/users/dto/update-user.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsString, IsOptional, IsIn } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsOptional()
+  @IsString({ message: 'Role deve ser uma string' })
+  @IsIn(['user', 'admin', 'admin-master'], { message: 'Role deve ser "user", "admin" ou "admin-master"' })
+  role?: string;
+}
